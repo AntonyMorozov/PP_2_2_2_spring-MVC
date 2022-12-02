5af50268792e36be772dca2c9b568dcad2707583
AntonyMorozov <moroztoni91@mail.ru>
2022-12-02 23:52:39 +0400

    2.2.2-2

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -12,26 +12,32 @@ public class Car {
     }
 
     public String getModel() {
+
         return model;
     }
 
     public void setModel(String model) {
+
         this.model = model;
     }
 
     public int getSeries() {
+
         return series;
     }
 
     public void setSeries(int series) {
+
         this.series = series;
     }
 
     public String getColor() {
+
         return color;
     }
 
     public void setColor(String color) {
+
         this.color = color;
     }
 }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,11 +1,23 @@
 package web.service;
-
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
-@Component
-public interface CarService {
+@Service
+public abstract class CarService {
 
-    List<Car> getListOfCar(int count);
+    public static final List<Car> resultList = new ArrayList<>();
+
+    {
+
+        resultList.add(new Car("BMW", 123, "black"));
+        resultList.add(new Car("Chery", 7, "white"));
+        resultList.add(new Car("Chevrolet", 23, "yellow"));
+        resultList.add(new Car("Audi", 15, "grey"));
+        resultList.add(new Car("Jeep", 24, "blue"));
+    }
+// Так?
+    public abstract List<Car> getListOfCar(int count);
+
 }
